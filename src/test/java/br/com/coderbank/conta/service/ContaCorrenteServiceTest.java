29df64fbedcd2e1000a4ff784e12d322b83f4b14
Geovane Freitas <geovanefreitas@lampp-it.com.br>
2022-11-14 10:28:36 -0300

    Finalizacao dos testes para o service

--- src/test/java/br/com/coderbank/conta/service/ContaCorrenteServiceTest.java
+++ src/test/java/br/com/coderbank/conta/service/ContaCorrenteServiceTest.java
@@ -1,7 +1,10 @@
 package br.com.coderbank.conta.service;
 
 import br.com.coderbank.conta.domain.Cliente;
+import br.com.coderbank.conta.domain.ContaCorrente;
+import br.com.coderbank.conta.dto.ContaCorrenteDTO;
 import br.com.coderbank.conta.repository.ClienteRepository;
+import br.com.coderbank.conta.repository.ContaCorrenteRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +13,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -22,6 +27,10 @@ public class ContaCorrenteServiceTest {
 
     @Mock
     private ClienteRepository clienteRepository;
+
+    @Mock
+    private ContaCorrenteRepository contaCorrenteRepository;
+
 
     @Mock
     private ObjectMapper objectMapper;
@@ -50,5 +59,45 @@ public class ContaCorrenteServiceTest {
 //        para criar um cliente
 
         verify(clienteRepository, times(1)).save(clienteFake);
+    }
+
+    @Test
+    void deveObterTodasAsContas() {
+//        Cenário: criação das variáveis e objetos fakes que vamos precisar
+//        O que eu preciso para poder simular esse teste ? preciso de uma lista de contas
+//        para quando eu chamar o obterTodasAsContas
+
+        var contasFake = Arrays.asList(
+                ContaCorrente.builder()
+                        .id(UUID.fromString("23e4567-e89b-12d3-a456-426614174000"))
+                        .build(),
+                ContaCorrente.builder()
+                        .id(UUID.fromString("23e4567-e89b-12d3-a456-426614174000"))
+                        .build()
+        );
+
+        var contasFakeDTOEsperada = Arrays.asList(
+                ContaCorrenteDTO.builder()
+                        .id(UUID.fromString("23e4567-e89b-12d3-a456-426614174000"))
+                        .build(),
+                ContaCorrenteDTO.builder()
+                        .id(UUID.fromString("23e4567-e89b-12d3-a456-426614174000"))
+                        .build()
+        );
+
+        when(contaCorrenteRepository.findAll()).thenReturn(contasFake);
+
+//        Ação: é a chamada para  o método que queremos testar. Quem
+////        queremos testar ? obterContas
+
+        var contasDTORetornada = contaCorrenteService.obterContas();
+
+//        Validação: é onde vamos verificar o resultado da ação, baseado no cenário
+//        criado. É aqui que vamos verificar se o resultado da ação está de acordo
+//        com o esperado.
+//        O que esperamos ? que seja retornada uma lista de contas no formato DTO
+
+        assertEquals(contasFakeDTOEsperada, contasDTORetornada);
+
     }
 }
